45e83d3d10c1369349d0c9de8dd92eb4c49221a7
biruk <birukbeletu@gmail.com>
2022-06-06 13:48:12 -0700

    ddflskj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main (void)
 	   long int o;
 	   long long int r;
 	   float t;
-	  printf("size of a char: %lu bytes(u)\n",(unsigned long)sizeof(i));
+	  printf("size of a char: %lu bytes(s)\n",(unsigned long)sizeof(u));
 	  printf("size of an int: %lu bytes(s)\n",(unsigned long)sizeof(i));
 	  printf("size of a long int: %lu bytes(s)\n",(unsigned long)sizeof(o));
-	  printf("size of a long long: %lu bytes(s)\n",(unsigned long)sizeof(r));
+	  printf("size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(r));
 	  printf("size of a float: %lu bytes(s)\n",(unsigned long)sizeof(t));
 	  
 
