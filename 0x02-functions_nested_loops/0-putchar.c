d99d623d69048119d3e838ea789f0d7b666bd905
biruk <birukbeletu@gmail.com>
2022-06-07 13:06:33 -0700

    ddfshlksjh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,51 @@
-#include <stdio.h>
+#include <unistd.h>
 
-/**
- *  * main - Entry point
- *   * Return:0
- *    */
 
-int main(void)
+
+int _putchar(char c)
+
 {
-	char c[10] = "Holberton";
-	int i = 0;
-	while (i < 9)
-	{
-		putchar(c[i]);
-		i++;
-	}
-	putchar('\n');
-	return (0);
+
+		return (write(1, &c, 1));
+
 }
+
+
+
+int main ()
+
+{
+
+
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+
+
+	return 0;
+
+
+
+
+
+}
+
+
