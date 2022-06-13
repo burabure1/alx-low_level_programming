a8c0a8dbcbce731611574798f71e20a2ad9dd12b
burabure1 <104757321+burabure1@users.noreply.github.com>
2022-06-13 12:25:31 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,26 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * _strcpy - function that copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest.
- * @dest: destination.
- * @src: source
- * Return: the pointer to dest.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int count = 0;
+#include "main.h"
 
-	while (count >= 0)
+/**
+ * print_array - print elements of an array
+ *
+ * @a: The array
+ * @n: The number of elements in the array
+ *
+ * Return: void
+ */
+
+void  print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
-			break;
-		count++;
+		printf("%d", a[i]);
+
+		if (i + 1 != n)
+			printf(", ");
 	}
-	return (dest);
+
+	printf("\n");
 }
