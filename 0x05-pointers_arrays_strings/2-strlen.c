f5af43661cef4f39e9a78279fd763e9375008a71
biruk <birukbeletu@gmail.com>
2022-06-13 10:06:32 -0700

    jkhsd

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strien -lenght of string 
+ * 0s: A pointer to an int that wil be chged/update
+ *
+ * return:void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+	while (s[u] != '\0)
+	{u++;
+	}return (u);
+}
