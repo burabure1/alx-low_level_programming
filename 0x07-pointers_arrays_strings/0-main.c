25b258abf0a72c70a87308cd1620ee51aadfb72f
biruk <birukbeletu@gmail.com>
2022-06-21 03:40:33 -0700

    this

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,53 +1,53 @@
-IN_H                                               
+fndef _MAIN_H_
 
-                                                             
 
-                                                             
 
-                                                             
+#define _MAIN_H_
 
-char *_memset(char *s, char b, unsigned int n);              
 
-                                                             
 
-char *_memcpy(char *dest, char *src, unsigned int n);        
+char *_memset(char *s, char b, unsigned int n);
 
-                                                             
 
-char *_strchr(char *s, char c);                              
 
-                                                             
+char *_memcpy(char *dest, char *src, unsigned int n);
 
-unsigned int _strspn(char *s, char *accept);                 
 
-                                                             
 
-char *_strpbrk(char *s, char *accept);                       
+char *_strchr(char *s, char c);
 
-                                                             
 
-char *_strstr(char *haystack, char *needle);                 
 
-                                                             
+unsigned int _strspn(char *s, char *accept);
 
-int _putchar(char);                                          
 
-                                                             
 
-void print_chessboard(char (*a)[8]);                         
+char *_strpbrk(char *s, char *accept);
 
-                                                             
 
-void print_diagsums(int *a, int size);                       
 
-                                                             
+char *_strstr(char *haystack, char *needle);
 
-void set_string(char **s, char *to);                         
 
-                                                             
 
-                                                             
+int _putchar(char);
 
-                                                             
 
-#endif     
+
+void print_chessboard(char (*a)[8]);
+
+
+
+void print_diagsums(int *a, int size);
+
+
+
+void set_string(char **s, char *to);
+
+
+
+
+
+
+
+#endif
