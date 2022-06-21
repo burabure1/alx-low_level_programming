9423bde3e2718278b841a20707d2d9eb7cd6844c
biruk <birukbeletu@gmail.com>
2022-06-21 03:58:11 -0700

    thid

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filleds .
- * Return: the pointer to dest.
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: costant byte
+ * @n: bytes filled
+ * return: Dest
  */
-
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, usigned int n)
 {
-	unsigned int i;
+	usingned int i;
 
 	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
+		*(s + i) = b;
 	return (s);
 }
