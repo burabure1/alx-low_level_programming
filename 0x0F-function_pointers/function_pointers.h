f0310d82dd1e3b840164377b82ac7677945393b7
burabure1 <104757321+burabure1@users.noreply.github.com>
2022-07-07 02:20:40 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,25 +1,14 @@
-#include "function_pointers.h"
-/**
- * int_index - searches for an integer
- * @array: pointer to an int
- * @size: pointer to an int
- * @cmp: pointer to a function
- * Return: integer
- */
-int int_index(int *array, int size, int (*cmp)(int))
-{
-int result;
-int i;
-if (array && cmp)
-{
-for (i = 0; i < size; i++)
-{
-result = cmp(array[i]);
-if (result != 0)
-{
-return (i);
-}
-}
-}
-return (-1);
-}
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+#endif
